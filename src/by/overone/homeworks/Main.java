139732777f033c07178011f2b38c10df377a2db4
Pavel Ivanchik <ivanchik.pal@gmail.com>
2021-10-30 11:06:10 +0300

    learn try for arithmetic exc

--- src/by/overone/homeworks/Main.java
+++ src/by/overone/homeworks/Main.java
@@ -2,6 +2,17 @@ package by.overone.homeworks;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main");
+        int a = 10;
+        int b = 2;
+
+        try {
+            System.out.println(a/b);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Division by zero/ sorry");
+        }
+        finally {
+            System.out.println("Goodbye");
+        }
     }
 }
