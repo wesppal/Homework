c7654a278ee66a005c8463f730940e5b161b8972
Pavel Ivanchik <ivanchik.pal@gmail.com>
2021-10-31 12:19:59 +0300

    test without a total

--- src/by/overone/Main.java
+++ src/by/overone/Main.java
@@ -1,12 +1,23 @@
 package by.overone;
 
 
+import by.overone.game.GameField;
+import by.overone.game.User;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
-
+        Scanner sc = new Scanner(System.in);
+        int a;
+        GameField.printHelpField();
+        while(true) {
+            System.out.println("Your step - ");
+            a = sc.nextInt();
+            User.step(a);
+            GameField.printField();
+        }
 
     }
 }
