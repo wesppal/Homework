062e19f812afc6ec76d28d3df8ba3830374cdd84
Pavel Ivanchik <ivanchik.pal@gmail.com>
2021-10-31 19:23:18 +0300

    Remake all. Fixed occupied cell

--- src/by/overone/game/User.java
+++ src/by/overone/game/User.java
@@ -2,18 +2,30 @@ package by.overone.game;
 
 
 public class User {
+
     private static char figureUser = 'X';
 
     public static void step(int cellNumber) {
-        if ((GameField.fieldForGame[cellNumber] != 'O') && (GameField.fieldForGame[cellNumber] != figureUser)) {
-            GameField.step(cellNumber, figureUser);
-            Ai.stepByEasyAi();
+        if ((cellNumber >= 0) && (cellNumber < 9)) {
+
+            for (Cell cell : GameField.fieldEmpty) {
+                if (cell.index == cellNumber) {
+                    GameField.fieldEmpty.remove(cell);
+                    GameField.step(cellNumber, figureUser);
+
+                    if (GameField.fieldEmpty.size() > 1) {
+                        Ai.stepByEasyAi();
+                    }
+
+                    break;
+                }
+            }
         } else {
-            System.out.println("Sorry, this cage is busy. Choose another one.");
+            System.out.println("Sorry, there is no given cell, choose another one.");
         }
 
-
+        if (GameField.fieldForGame.get(cellNumber).figure != figureUser) {
+            System.out.println("Sorry, this cage is busy. Choose another one.");
+        }
     }
-
-
 }
