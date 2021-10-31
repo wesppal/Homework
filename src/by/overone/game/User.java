4bf63f2b7ea2685bec574e95765570e1b3642f63
Pavel Ivanchik <ivanchik.pal@gmail.com>
2021-10-31 12:28:24 +0300

    fix repeat move

--- src/by/overone/game/User.java
+++ src/by/overone/game/User.java
@@ -1,13 +1,18 @@
 package by.overone.game;
 
 
-
 public class User {
     private static char figureUser = 'X';
 
-    public static void step (int cellNumber) {
-        GameField.step(cellNumber,figureUser);
-        Ai.stepByEasyAi();
+    public static void step(int cellNumber) {
+        if ((GameField.fieldForGame[cellNumber] != 'O') && (GameField.fieldForGame[cellNumber] != figureUser)) {
+            GameField.step(cellNumber, figureUser);
+            Ai.stepByEasyAi();
+        } else {
+            System.out.println("Sorry, this cage is busy. Choose another one.");
+        }
+
+
     }
 
 
