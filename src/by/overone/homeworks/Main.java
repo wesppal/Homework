5f75a1ddfb8b3412e34e97da0376a5f85ab7ef52
Pavel Ivanchik <ivanchik.pal@gmail.com>
2021-11-10 17:07:14 +0300

    add method transform and check

--- src/by/overone/homeworks/Main.java
+++ src/by/overone/homeworks/Main.java
@@ -2,16 +2,21 @@ package by.overone.homeworks;
 
 public class Main {
     public static void main(String[] args) {
-        Num<Integer> integerNum = new Num(5);
-        System.out.println(integerNum.sum(5));
-        Num<Double> doubleNum = new Num(5.0);
-        System.out.println(doubleNum.sum(5.0));
-        Num<Float> floatNum = new Num(1f);
-        System.out.println(floatNum.sum(2f));
-        Num<Byte> byteNum = new Num((byte) 1);
-        System.out.println(byteNum.sum((byte) 1));
-        Num<Short> shortNum = new Num((short) 2);
+        Num<Integer> integerNum = new Num(900000000);
+        System.out.println(integerNum.sum(900000000));
+
+        Num<Double> doubleNum = new Num(500000000000000000000000000000000000000000000000000000.0);
+        System.out.println(doubleNum.sum(50000000000000.0));
+
+        Num<Float> floatNum = new Num(100000000f);
+        System.out.println(floatNum.substraction(2000000f));
+
+        Num<Byte> byteNum = new Num((byte) 128);
+        System.out.println(byteNum.substraction((byte) 0));
+
+        Num<Short> shortNum = new Num((short) 2000000000);
         System.out.println(shortNum.sum((short) 2));
+
         Num<Long> longNum = new Num(9000000000000000000L);
         System.out.println(longNum.sum(9000000000000000000L));
     }
--- src/by/overone/homeworks/Num.java
+++ src/by/overone/homeworks/Num.java
@@ -5,25 +5,94 @@ public class Num<A extends Number> {
     private A value;
 
     public Num(A value) {
-        this.value = value;
+        if (universalCheck(value) == false) {
+            this.value = value;
+        } else {
+            System.out.println("Error.");
+        }
+
     }
 
 
     public A sum(A added) {
         Double sum = value.doubleValue() + added.doubleValue();
-        if (value instanceof Integer){
-            return (A)(Integer)sum.intValue();
-        }
-        if (value instanceof Short){
-            return (A)(Short)sum.shortValue();
-        }
-        if (value instanceof Long){
-            return (A)(Long)sum.longValue();
-        }
-
-        return (A) sum;
+        return universalTransform(value, sum);
     }
 
+    public A substraction(A deduct) {
+        Double result = value.doubleValue() - deduct.doubleValue();
+        if (universalCheck(deduct)){
+            if (universalCheck((A)result)){
+
+            }
+        }
+        return universalTransform(value, result);
+    }
+
+
+    private A universalTransform(A value, Double result) {
+        if (value instanceof Integer) {
+            return (A) (Integer) result.intValue();
+        }
+        if (value instanceof Short) {
+            return (A) (Short) result.shortValue();
+        }
+        if (value instanceof Long) {
+            return (A) (Long) result.longValue();
+        }
+        if (value instanceof Byte) {
+            return (A) (Byte) result.byteValue();
+        }
+        if (value instanceof Float) {
+            return (A) (Float) result.floatValue();
+        }
+        return (A) result;
+    }
+
+    private boolean universalCheck(A value) {
+        if (value instanceof Byte) {
+            if ((value.byteValue() < Byte.MIN_VALUE) || (value.byteValue() > Byte.MAX_VALUE) || (value == null)) {
+                System.out.println("Sorry, out of bounds");
+            }
+            return ((value.byteValue() < Byte.MIN_VALUE) || (value.byteValue() > Byte.MAX_VALUE) || (value == null));
+        }
+
+        if (value instanceof Short) {
+            if ((value.shortValue() < Short.MIN_VALUE) || (value.shortValue() > Short.MAX_VALUE)) {
+                System.out.println("Sorry, out of bounds");
+            }
+            return ((value.shortValue() < Short.MIN_VALUE) || (value.shortValue() > Short.MAX_VALUE));
+        }
+
+        if (value instanceof Integer) {
+            if ((value.intValue() < Integer.MIN_VALUE) || (value.intValue() > Integer.MAX_VALUE)) {
+                System.out.println("Sorry, out of bounds");
+                return ((value.intValue() < Integer.MIN_VALUE) || (value.intValue() > Integer.MAX_VALUE));
+            }
+        }
+
+        if (value instanceof Long) {
+            if ((value.longValue() < Long.MIN_VALUE) || (value.longValue() > Long.MAX_VALUE)) {
+                System.out.println("Sorry, out of bounds");
+                return ((value.longValue() < Long.MIN_VALUE) || (value.longValue() > Long.MAX_VALUE));
+            }
+        }
+
+        if (value instanceof Float) {
+            if ((value.floatValue() < Float.MIN_VALUE) || (value.floatValue() > Float.MAX_VALUE)) {
+                System.out.println("Sorry, out of bounds");
+                return ((value.floatValue() < Float.MIN_VALUE) || (value.floatValue() > Float.MAX_VALUE));
+            }
+        }
+
+        if (value instanceof Double) {
+            if ((value.doubleValue() < Double.MIN_VALUE) || (value.doubleValue() > Double.MAX_VALUE)) {
+                System.out.println("Sorry, out of bounds");
+                return ((value.doubleValue() < Double.MIN_VALUE) || (value.doubleValue() > Double.MAX_VALUE));
+            }
+        }
+        return false;
+    }
 
     @Override
     public String toString() {
