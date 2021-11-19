73260d6be75522002469f48d42135c024b0bc503
Pavel Ivanchik <ivanchik.pal@gmail.com>
2021-11-19 20:28:17 +0300

    create binary

--- src/by/overone/homeworks/Main.java
+++ src/by/overone/homeworks/Main.java
@@ -4,19 +4,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
+import java.util.stream.IntStream;
+
+import static java.util.Arrays.stream;
 
 public class Main {
     public static void main(String[] args) {
         Predicate<Integer> positive = x -> x >= 0;
         Integer[] arr = {2, 5, -5, -3, 2, 0, -5, -6, -7, 22};
-        Arrays.stream(arr).filter(positive).forEach(System.out::println);
+        stream(arr).filter(positive).forEach(System.out::println);
         System.out.println();
 
         UnaryOperator<Integer> pow = x -> x * x;
 //        Arrays.stream(arr).forEach(x -> System.out.println(pow.apply(x)));
-        Arrays.stream(arr).map(x -> pow.apply(x)).forEach(System.out::println);
+        stream(arr).map(x -> pow.apply(x)).forEach(System.out::println);
         System.out.println();
 
         List<String> str = new ArrayList<>();
