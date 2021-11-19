bc4a0a9c2f7fb047dfce7ee6d06d9727a9a70613
Pavel Ivanchik <ivanchik.pal@gmail.com>
2021-11-19 09:08:11 +0300

    Predicate >=0

--- src/by/overone/homeworks/Main.java
+++ src/by/overone/homeworks/Main.java
@@ -1,7 +1,12 @@
 package by.overone.homeworks;
 
+import java.util.Arrays;
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
-
+        Predicate<Integer> positive = x -> x >= 0;
+        Integer[] arr = {2,5,-5,-3,2,0,-5,-6,-7,22};
+        Arrays.stream(arr).filter(positive).forEach(System.out::println);
     }
 }
