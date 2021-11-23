abed767e0c630e458d8440c50aa6a68f12d77b38
Pavel Ivanchik <ivanchik.pal@gmail.com>
2021-11-23 13:40:43 +0300

    create method unique word

--- src/by/overone/OpText.java
+++ src/by/overone/OpText.java
@@ -26,4 +26,27 @@ public class OpText {
         }
         System.out.println("Самое повторяющееся слово: " + s + " (встречается " + h.get(s) + " раз)");
     }
+
+    public static void repeatingWordMin(String text) {
+        String[] textArrayWords = text.split("\\s");
+        HashMap<String, Integer> h = new HashMap<>();
+        for (int i = 0; i < textArrayWords.length; i++) {
+            if (h.containsKey(textArrayWords[i])) {
+                h.replace(textArrayWords[i], h.get(textArrayWords[i]) + 1);
+            } else {
+                h.put(textArrayWords[i], 1);
+            }
+        }
+        String s = null;
+        Integer min = h.get(textArrayWords[0]);
+        for (String w : h.keySet()) {
+            if (h.get(w) != null) {
+                if (Math.min(min, h.get(w)) <= min) {
+                    s = w;
+                }
+                min = Math.min(min, h.get(w));
+            }
+        }
+        System.out.println("Самое редкое слово: " + s + " (встречается " + h.get(s) + " раз)");
+    }
 }
