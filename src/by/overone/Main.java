28e9bfcb238c724e23a67bc408c4b92b8b5df559
Pavel Ivanchik <ivanchik.pal@gmail.com>
2021-11-23 12:48:19 +0300

    add new txt

--- src/by/overone/Main.java
+++ src/by/overone/Main.java
@@ -1,4 +1,20 @@
 package by.overone;
 
+import java.io.*;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        File file = new File("D:/Учёба Java/Homework/src/by/overone/Толстой-Лев.-Война-и-мир.Том-1-и-2.txt");
+        InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(file), "UTF-8");
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+        int ch;
+        StringBuilder builder = new StringBuilder();
+
+        while ((ch = reader.read()) != -1) {
+            builder.append((char) ch);
+        }
+        System.out.println(builder);
+
+
+    }
 }
