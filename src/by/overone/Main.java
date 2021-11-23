15ef8096ea7cf556c293cb27ccff7ddd8ec7e19c
Pavel Ivanchik <ivanchik.pal@gmail.com>
2021-11-23 14:42:30 +0300

    release method findYears

--- src/by/overone/Main.java
+++ src/by/overone/Main.java
@@ -1,10 +1,12 @@
 package by.overone;
 
 import java.io.*;
+import java.util.List;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("D:/Учёба Java/Homework/src/by/overone/Толстой-Лев.-Война-и-мир.Том-1-и-2.txt");
+        File file = new File("D:/Учёба Java/Homework/src/by/overone/Толстой Лев. Война и мир. Том 1 и 2.txt");
         InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(file), "UTF-8");
         BufferedReader reader = new BufferedReader(inputStreamReader);
         int ch;
@@ -15,10 +17,21 @@ public class Main {
         }
         System.out.println(text);
 
-        File tempFile = new File("D:/Учёба Java/Homework/src/by/overone/tempTxt.txt");
-        tempFile.mkdir();
-        OpText.repeatingWord(text.toString(), 5);
+//        File tempFile = new File("tempTxt.txt");
 
-        OpText.repeatingWordMin(text.toString());
+        OpText.repeatingWord(text.toString(), 5);
+        OpText.uniqueWord(text.toString());
+        OpText.longWord(text.toString());
+        List<String> years = OpText.findYears(text.toString());
+        System.out.println("Количество встречающихся упоминаний годов в тексте - " + years.size()
+                + ". Упомянутые годы:");
+        if (years.size() == 0) {
+            System.out.println("не встеритось");
+        }
+
+        TreeSet<String> yearsSet = new TreeSet<>(years);
+        for (String s : yearsSet) {
+            System.out.print(s + "; ");
+        }
     }
 }
--- src/by/overone/OpText.java
+++ src/by/overone/OpText.java
@@ -1,11 +1,13 @@
 package by.overone;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 
 public class OpText {
     public static void repeatingWord(String text, int numberOfLettersInAWord) {
-        String[] textArrayWords = text.split(" ");
+        String[] textArrayWords = text.split("\\s");
         HashMap<String, Integer> h = new HashMap<>();
         for (int i = 0; i < textArrayWords.length; i++) {
             if (h.containsKey(textArrayWords[i])) {
@@ -27,7 +29,7 @@ public class OpText {
         System.out.println("Самое повторяющееся слово: " + s + " (встречается " + h.get(s) + " раз)");
     }
 
-    public static void repeatingWordMin(String text) {
+    public static void uniqueWord(String text) {
         String[] textArrayWords = text.split("\\s");
         HashMap<String, Integer> h = new HashMap<>();
         for (int i = 0; i < textArrayWords.length; i++) {
@@ -48,5 +50,40 @@ public class OpText {
             }
         }
         System.out.println("Самое редкое слово: " + s + " (встречается " + h.get(s) + " раз)");
+    }
+
+    public static void longWord(String text) {
+        String[] textArrayWords = text.split("\\s");
+        String temp = textArrayWords[0];
+        int max = textArrayWords[0].length();
+        for (String str : textArrayWords) {
+            if (str.length() >= max) {
+                max = Math.max(str.length(), max);
+                temp = str;
+            }
+        }
+        System.out.println("Самое длинное слово: " + temp);
+    }
+
+    public static List<String> findYears(String text) {
+        String[] textArrayWords = text.split("\\s");
+        List<String> years = new ArrayList<>();
+        for (String str : textArrayWords) {
+            if (isDigit(str)) {
+                if ((Integer.parseInt(str) > 1000) && (Integer.parseInt(str) < 2021)) {
+                    years.add(str);
+                }
+            }
+        }
+        return years;
+    }
+
+    private static boolean isDigit(String s) throws NumberFormatException {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
